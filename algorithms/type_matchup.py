8faad9fd86695036c5309773621364f8ab1fcfbd
Kaliber10 <dev.Kaliber@gmail.com>
2021-10-26 21:20:03 -0400

    Updated exit calls to python standard. Python exit calls were changed from "exit" to "sys.exit" as recommended by python.

--- algorithms/type_matchup.py
+++ algorithms/type_matchup.py
@@ -1,4 +1,5 @@
 #!usr/bin/env python3
+import sys
 
 class Type_Matchup ():
     Type_Data = {}
@@ -23,21 +24,21 @@ class Type_Matchup ():
                         self.Type_Data[r]['not'].append(value)
                     except KeyError:
                         print("Type " + r + " does not exist")
-                        exit()
+                        sys.exit()
                 for w in input[index]['weaknesses']:
                     try:
                         self.Type_Data[value]['weaknesses'].append(w)
                         self.Type_Data[w]['super'].append(value)
                     except KeyError:
                         print("Type " + w + " does not exist")
-                        exit()
+                        sys.exit()
                 for i in input[index]['immunities']:
                     try:
                         self.Type_Data[value]['immunities'].append(i)
                         self.Type_Data[i]['doesnt'].append(value)
                     except KeyError:
                         print("Type " + i + " does not exist")
-                        exit()
+                        sys.exit()
             except KeyError as e:
                 print("Missing '" + str(e) + "' Entry")
-                exit()
+                sys.exit()
