d042db820c42a6a64f6a894e8d2f8786741622fc
Kaliber10 <dev.Kaliber@gmail.com>
2021-10-28 17:24:13 -0400

    Started using Type_Matchup calls. Type_Matchup calls were made from the matchup_generator for testing. The data will be shared in Type_Matchup to be used by the plugins.

--- matchup_generator.py
+++ matchup_generator.py
@@ -1,6 +1,7 @@
 import enum
 import json
 import math
+import algorithms.type_matchup as tm
 
 # Generate the matchup chart for the types.
 # This is not algorithm dependent.
@@ -144,7 +145,6 @@ def generate_table():
         print(" " * math.floor(fill_def/2) + str(arr[val.value][0]) + " " * math.ceil(fill_def/2), end="|")
         print(" " * math.floor(fill_off/2) + str(arr[val.value][1]) + " " * math.ceil(fill_off/2) + "|")
         
-
 #TODO Weighted score: Everything over 1 has a higher weight.
 #Open json file
 #TODO Make it so it can open different json files
@@ -216,8 +216,10 @@ for val in Types:
 
 #TODO for algorithm file, run through a queue of equations to get to the final one.
 #TODO create an algorithm array to store all the scores generated to be used by other algorithms.
-generate_table()
+#generate_table()
 
-generate_matchups()
+#generate_matchups()
+type_data = tm.Type_Matchup ()
+type_data.generate_data (idata)
 
 #TODO Create an algorithm file so that people can create and share their own
