169872dd105654d882bf1d12a5402f4b893585a9
Kaliber10 <dev.Kaliber@gmail.com>
2022-04-23 11:27:31 -0400

    generate_matchups returns dictionary.
    The generate_matchups function now returns a dictionary consisting of
    the types and a 2d array of the values. It does not print the chart. The
    printing of the chart is handled in the main function.

--- matchup_generator.py
+++ matchup_generator.py
@@ -172,29 +172,27 @@ def generate_data (input):
     return Type_Data
     
 # Generate the matchup chart for the types.
-# This is not algorithm dependent.
-# This should move to type_matchup.
-def generate_matchups(Data):
-    temp_filler = "   "
-    # Print the top of the table.
-    print ("   |", end="")
+def generate_matchups(Data : dict):
+    chart = {"header" : [],
+             "matchup" : []}
+    # Probably alphabetize the keys to make it easier
+    # to find duplicates.
     for type in Data.keys():
-        print (type[:3].capitalize(), end="|") # Print the first 3 characters of the type
-    print("")
-    # Print the left side of the table.
-    for type in Data.keys():
-        print (type[:3].capitalize(), end="|")
-        for opp_type in Data.keys():
-            if opp_type in Data[type]['super']:
-                print(" 2 |", end="")
-            elif opp_type in Data[type]['reduced']:
-                print("0.5|", end="")
-            elif opp_type in Data[type]['none']:
-                print(" 0 |", end="")
-            else:
-                print(" 1 |", end="")
-
-        print("")
+        if type[0:3] in chart["header"]:
+            chart["header"].append(type[0:2] + type[4])
+            print("Error: Potential Duplicate Names")
+        else:
+            chart["header"].append(type[0:3])
+    type_index_map = {type: index for index, type in enumerate(Data.keys())}
+    for index, type in enumerate(Data.keys()):
+        chart['matchup'].append([1 for i in Data.keys()])
+        for j in Data[type]['super']:
+            chart['matchup'][index][type_index_map[j]] = 2
+        for j in Data[type]['reduced']:
+            chart['matchup'][index][type_index_map[j]] = 0.5
+        for j in Data[type]['none']:
+            chart['matchup'][index][type_index_map[j]] = 0
+    return chart
 
 def find_valid_plugins(pkg_space):
     #1. Find the plugins in the folder
@@ -362,7 +360,19 @@ def main():
         sys.exit(0)
     idata = matchup_list[selected_matchup]['matchup']
     data = generate_data (idata)
-    generate_matchups(data['data'])
+    chart = generate_matchups(data['data'])
+    print("   |", end="")
+    for type in chart['header']:
+        print (type.capitalize(), end="|")
+    print("")
+    for ind in range(len(chart['matchup'])):
+        print (chart['header'][ind].capitalize(), end="|")
+        for jnd in range(len(chart['matchup'][ind])):
+            val = str(chart['matchup'][ind][jnd])
+            if len(val) < 3:
+                val = ' ' * math.ceil((3 - len(val)) / 2) + val + ' ' * math.floor((3 - len(val)) / 2)
+            print (val + "|", end="")
+        print("")
     print(alg_entries[alg]['name'])
     alg = alg_entries[alg]['class']()
     try:
